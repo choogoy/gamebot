6d58f46df638c38e831845fb3e6786f065368e14
choogoy <victormm0312@yandex.ru>
2021-04-08 13:21:16 +0300

    gamebot усложненное задание

--- script.js
+++ script.js
@@ -12,34 +12,60 @@ const isNumber = function(n) {
 
 // функция запуска бота
 function gameBot() {
+  let attempts = 10; // количество попыток
   const random = Math.ceil(Math.random()*100); // загаданное число от 1 до 100
   
-  //проверка введенного числа, random - замкнуто для checkNumber
+  // действие при исчерпании попыток
+  const checkAttempts = function() {
+    if (attempts <= 1) {
+      let newGameQuestion = confirm('Попытки закончились, хотите сыграть еще?');
+      if (newGameQuestion === false) {
+        return false;
+      } else {
+        gameBot();
+      }
+    }
+  };
+
+  //проверка введенного числа, random и attempts - замкнуты для checkNumber
   const checkNumber = function(num) {
-    if (num === null) {
-      alert ('Игра окончена');
-      return;
+    if (checkAttempts() === false) {
+      alert ('До встречи!');
+      return;        
+    } else if (num === null) {
+        alert ('Игра окончена');
+        return;
     } else {
-      if (isNumber(num)) {
+      if (isNumber(num) && attempts > 0) {
+        attempts--;
         if (+num === random) {
-          alert('Поздравляю, Вы угадали!!!');
-          return;
+          let questionCongratulations = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще');
+          if (!questionCongratulations) {
+            return;
+          } else {
+            gameBot();
+          }
         }
         if (+num > random) {
-          alert('Загаданное число меньше');
+          alert(`Загаданное число меньше, осталось ${attempts} попыток`);
           checkNumber(question());
         }
         if (+num < random) {
-          alert('Загаданное число больше');
+          alert(`Загаданное число больше, осталось ${attempts} попыток`);
           checkNumber(question());
-        }   
+        }
       } else {
-        alert('Введи число!');
+        alert(`Введите число`);
         checkNumber(question());
-      } 
+      }
     }
   };
-  checkNumber(question());
+
+  // попытка угадать число при наличии потыток
+  if (attempts > 0) {
+    checkNumber(question());
+  }
+
 }
 
 gameBot();
